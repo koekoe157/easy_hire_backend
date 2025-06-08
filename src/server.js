c105e9e84405dbc1880ecac63a5805428af4dcd3
Claryngweyee <161452606+Claryquinnzle22@users.noreply.github.com>
2025-06-08 21:30:12 +0700

    host

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ import mainRouter from "./routes/mainRouter.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const HOST = process.env.HOST || "0.0.0.0";
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json()); 
+app.use(express.json());
 app.use("/api/v1", mainRouter);
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`🚀 Server running at ${HOST}:${PORT}`);
 });
