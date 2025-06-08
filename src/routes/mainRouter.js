2f0b46515303562da443226990dc83337dc4bbc5
Claryngweyee <161452606+Claryquinnzle22@users.noreply.github.com>
2025-06-08 18:02:06 +0700

    pending feature

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import testRouter from "./testRouter.js";
-import userRouter from "./userRouter.js"; 
+import userRouter from "./userRouter.js";
 import jobRouter from "./jobRouter.js";
+import applyRouter from "./applyRouter.js"; // Assuming applyRouter is defined elsewhere
 
 const mainRouter = Router();
 
 mainRouter.use("/test", testRouter);
-mainRouter.use("/users", userRouter); 
+mainRouter.use("/users", userRouter);
 mainRouter.use("/jobs", jobRouter);
+mainRouter.use("/apply", applyRouter); // Assuming applyRouter is defined elsewhere
 
 export default mainRouter;
